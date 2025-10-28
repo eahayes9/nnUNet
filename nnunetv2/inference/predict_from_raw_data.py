abf3beba5ccb324ccdfec52e4b88e0a9570931b3
Elizabeth A. Hayes <eahayes@redhawk.org>
2025-10-28 07:46:04 -0600

    increase value_scaling_factor a bit

--- nnunetv2/inference/predict_from_raw_data.py
+++ nnunetv2/inference/predict_from_raw_data.py
@@ -591,7 +591,7 @@ class nnUNetPredictor(object):
 
             if self.use_gaussian:
                 gaussian = compute_gaussian(tuple(self.configuration_manager.patch_size), sigma_scale=1. / 8,
-                                            value_scaling_factor=2,
+                                            value_scaling_factor=10,
                                             device=results_device)
             else:
                 gaussian = 1
